19766d395290e6b925eb9952506019c02e6ab73f
Alonso <acabretpaz@gmail.com>
2021-07-04 13:26:41 -0400

    primera version de endpoint discover movies

--- controllers/movies.js
+++ controllers/movies.js
@@ -25,14 +25,26 @@ moviesRouter.get("/", (request, response) => {
 
 moviesRouter.get("/search", async (request, response) => {
   // const { query, language } = request.body
-  const { query, language } = request.query
-
+  const { query, language } = request.query;
 
   const results = await movieService.queryMovies({ query, language });
 
   return response.json(results);
 });
 
-moviesRouter.get("/");
+moviesRouter.get("/discover", async (request, response) => {
+  const { genre, language, voteGte, voteLte, dateGte } = request.query;
+
+  const results = await movieService.discoverMovies({
+    genre,
+    language,
+    voteGte,
+    voteLte,
+    dateGte,
+  });
+
+  return response.json(results)
+
+});
 
 module.exports = moviesRouter;
--- services/movies.js
+++ services/movies.js
@@ -1,19 +1,37 @@
 const axios = require("axios");
-const config = require("../utils/config")
+const config = require("../utils/config");
 
-const baseURL = "https://api.themoviedb.org/3";
-const searchMovieURL = `${baseURL}/search/movie`
 const defaultLanguage = "en-US";
+const baseURL = "https://api.themoviedb.org/3";
 
+const searchMovieBaseURL = `${baseURL}/search/movie`;
+const discoverMovieBaseURL = `${baseURL}/discover/movie`;
 
 const queryMovies = ({ query, language = defaultLanguage }) => {
-  
-  const fullURL = `${searchMovieURL}?api_key=${config.API_KEY}&query=${query}&language=${language}`
+  const fullURL = `${searchMovieBaseURL}?api_key=${config.API_KEY}&query=${query}&language=${language}`;
   console.log(fullURL);
-  
-  return axios
-    .get(fullURL)
-    .then((response) => response.data);
+
+  return axios.get(fullURL).then((response) => response.data);
 };
 
-module.exports = { queryMovies };
+const discoverMovies = ({
+  language = defaultLanguage,
+  genre,
+  voteGte,
+  voteLte,
+  dateGte
+}) => {
+  const sortBy = "&sort_by=vote_average.desc";
+  const dateGteKey = `&release_date.gte=${dateGte}`
+  const ratingLteKey = `&vote_average.lte=${voteLte}`;
+  const ratingGteKey = `&vote_average.gte=${voteGte}`;
+  const languageKey = `&language=${language}`;
+  const genreKey = `&genre=${genre}`;
+  const qualityKeys = "&include_adult=false&page=1&vote_count.gte=1000"
+  const fullURL = `${discoverMovieBaseURL}?api_key=${config.API_KEY}${languageKey}${dateGteKey}${genreKey}${ratingGteKey}${ratingLteKey}${sortBy}${qualityKeys}`;
+
+  console.log(fullURL);
+  return axios.get(fullURL).then((response) => response.data);
+};
+
+module.exports = { queryMovies, discoverMovies };

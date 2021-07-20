461bc0ea445eeb88933d6a771f55e3bdbcafab0e
acabret <acabretpaz@gmail.com>
2021-07-20 12:47:10 -0400

    commit for test deployment with build dir

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const moviesRouter = require("./controllers/movies")
+const cors = require("cors");
+const moviesRouter = require("./controllers/movies");
 
-app.use(cors({origin:"http://localhost:3000"}))
+app.use(express.static("build"));
+app.use(cors({ origin: "http://localhost" }));
 app.use(express.json());
 app.use("/api/movies", moviesRouter);
-
 
 module.exports = app;

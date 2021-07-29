5d2b6ffa13321a7bea3492927e89b9ae391c71a6
acabret <acabretpaz@gmail.com>
2021-07-28 20:51:48 -0400

    modifidied defaults keys in the discover endpoint, results are ordered by average score

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const moviesRouter = require("./controllers/movies");
 
 app.use(express.static("build"));
-app.use(cors({ origin: "http://localhost" }));
+// app.use(cors({ origin: "http://localhost" }));
 app.use(express.json());
 app.use("/api/movies", moviesRouter);
 
